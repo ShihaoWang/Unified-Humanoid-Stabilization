f0b367c4bfa2abc0e159d464bb6e267990b6f854
Shihao_Wang <sw309@duke.edu>
2018-04-03 19:58:23 -0400

    Towards a simplified version

--- Humanoid_Physical_Parameter.m
+++ Humanoid_Physical_Parameter.m
@@ -1,17 +1,25 @@
 function p = Humanoid_Physical_Parameter()
 
+% Please DO NOT forget to run this function when each value get updated!
+
 % This function is used to give the values for the physical parameter
 % Assume that the mass is propotional to the link length
 
-% The item sequence is [foot, shank, thigh, body ,head, arm, forearm]
-l_foot = 0.2;         l_shank = 0.3;        l_thigh = 0.3;
-l_body = 0.35;        l_head = 0.15;        l_arm = 0.2;          
-l_forearm = 0.25;
-p.l = [l_foot; l_shank; l_thigh; l_body; l_head; l_forearm; l_arm];
+% The item sequence is [shank, thigh, body, arm, forearm]
+l_shank = 0.25;         l_thigh = 0.25;         l_body = 0.3; 
+l_arm = 0.2;            l_forearm = 0.25;
+
+p.l = [l_shank; l_thigh; l_body; l_forearm; l_arm];
 
 l2m = 10;  % 1m is 10 kg
+
 p.m = p.l *l2m;
+
+p.m_tot = sum(p.m);
+
 p.I = 1/3 * p.m .* p.l.*p.l;
+
+p.KJ2JI = 0.25;    % This is only for plot purpose
 
 end
 
